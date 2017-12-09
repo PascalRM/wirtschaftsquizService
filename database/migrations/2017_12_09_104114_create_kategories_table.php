8ce13c5c6f4e1617081ec4797d9b2ef5444692ef
Pascal.Ramanathan <pascal.ramanathan@stud.bbbaden.ch>
2017-12-09 11:43:35 +0100

    Erstellen von Model/Migration Kategorie

---
+++ database/migrations/2017_12_09_104114_create_kategories_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateKategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kategories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('kategorie');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kategories');
+    }
+}
