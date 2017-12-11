cab81a0db655cc4852c8099ca7cf67d9a1b51fc9
Pascal.Ramanathan <pascal.ramanathan@stud.bbbaden.ch>
2017-12-11 11:02:05 +0100

    FragenTableSeeder edit

--- database/seeds/FragenTableSeeder.php
+++ database/seeds/FragenTableSeeder.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Frage;
+use App\Fragebogen;
+use App\Eingabe;
+use App\Truefalse;
+use App\Multiplechoice;
 
 class FragenTableSeeder extends Seeder
 {
@@ -11,6 +16,31 @@ class FragenTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Fragebogen::create([
+            'name' => 'Fragebogen1'
+        ]);
+
+
+        for($i = 0; $i<5; $i++){
+            for($j = 0; $j<3; $j++){
+                Frage::create([
+                    'frage' => 'frage'.$i,
+                    'typ' => $j+1,
+                    'id_antwort'=> $j,
+                    'id_fragebogen' => 1,
+                ]);
+            }
+        }
+
+        Frage::create([
+            'frage' => 'frage',
+        ]);
+
+        for ($i = 0; $i < 5; $i++) {
+            Kategorie::create([
+                'kategorie' => "Kategorie".$i,
+                'id_user' => $i,
+            ]);
+        }
     }
 }
