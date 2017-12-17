e17233c8f28bf55fddcadda0bf2a1cf67dbb559a
Pascal.Ramanathan <pascal.ramanathan@stud.bbbaden.ch>
2017-12-17 11:32:26 +0100

    Frage / Fragebogen model Fillable hinz.

--- app/Frage.php
+++ app/Frage.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Frage extends Model
 {
-    //
+    protected $fillable = ['frage','typ','id_antwort','id_fragebogen'];
 }
--- app/Fragebogen.php
+++ app/Fragebogen.php
@@ -7,4 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Fragebogen extends Model
 {
     //
+    protected $fillable = ['name','id_kategorie','id_user'];
 }
