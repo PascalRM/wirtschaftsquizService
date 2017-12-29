5bef9a919af804e691e35ad5e6ba995a37042474
Pascal.Ramanathan <pascal.ramanathan@stud.bbbaden.ch>
2017-12-29 11:58:29 +0100

    Anpassen Handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -55,8 +55,5 @@ class Handler extends ExceptionHandler
         };
     }
 
-    protected function unauthenticated($request, AuthenticationException $exception)
-    {
-        return response()->json(['error' => 'Unauthenticated'], 401);
-    }
+
 }
