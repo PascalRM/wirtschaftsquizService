c3c37fa87b2db72c8c2e2b17df691515c6ce4532
unknown <pascal.ramanathan@gmail.com>
2017-12-29 20:58:49 +0100

    Anpassen Cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -15,7 +15,7 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
-        header('Access-Control-Allow-Origin: http://localhost:4200');
+        header('Access-Control-Allow-Origin: http://localhost:4200/');
         header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
         header('Access-Control-Allow-Headers: Content-Type, Accept, Authorization, X-Requested-With, Application');
         return $next($request);
