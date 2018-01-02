ffce9721483c9b192d2054d25070f07d99c0ef5a
unknown <pascal.ramanathan@gmail.com>
2018-01-02 13:42:44 +0100

    UserInfoController erstellt

---
+++ app/Http/Controllers/UserInfoController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+
+class UserInfoController extends Controller
+{
+    public function show($id)
+    {
+        return User::find($id);
+    }
+
+}
